5c872fb98406aa669bc7e7e28720822df89fecf3
shigeosapsycho <157628053+shigeosapsycho@users.noreply.github.com>
2024-09-23 19:57:51 -0700

    Add files via upload

--- Project Module 2/ArrayList.py
+++ Project Module 2/ArrayList.py
@@ -60,7 +60,8 @@ class ArrayList(List):
     def append(self, x: object):
         self.add(self.n, x)
 
-    def add(self, i: int, x: object):
+    def add(self, i: int, x: object): # If inserting into the first-half "shift" every element from j to j + i one bin to the left
+                                      # If inserting into the second-half "shift" every element from j + n - 1 to j + i one bin to the right
         '''
             add: shift one position all the items j>=i, insert an element 
             at position i of the list and increment the number of elements 
@@ -69,33 +70,33 @@ class ArrayList(List):
                 i: Index that is integer non negative and at most n
                 x: Object type, i.e., any object
         '''
-        if i < 0 or i > self.n:
+        if i < 0 or i > self.n: # Precondiiton
             raise IndexError()
-        if len(self.a) == self.n:
+        if len(self.a) == self.n: # Invariant - amortized runtime
             self.resize()
-        if i < self.n / 2:
+        if i < self.n / 2: # Shift elements to the left, inserting into the first-half
             self.j = (self.j - 1) % len(self.a)
             for k in range(0, i):
                 self.a[(k + self.j) % len(self.a)] = self.a[(k + self.j + 1) % len(self.a)]
         else:
-            for k in range(self.n, i, -1):
+            for k in range(self.n, i, -1): # Shift elements to the right, inserting into the second-half
                 self.a[(k + self.j) % len(self.a)] = self.a[(k + self.j - 1) % len(self.a)]
         self.a[(i + self.j) % len(self.a)] = x
         self.n += 1
 
     def remove(self, i: int) -> object:
-        if i < 0 or i >= self.n:
+        if i < 0 or i >= self.n: # Precondition
             raise IndexError()
-        x = self.a[(i + self.j) % len(self.a)]
-        if i < self.n / 2:
+        x = self.a[(i + self.j) % len(self.a)] # Temporary variable to store the value to be removed
+        if i < self.n / 2: # Shift elements to the right, removing from the first-half
             for k in range(i, 0, -1):
                 self.a[(k + self.j) % len(self.a)] = self.a[(k + self.j - 1) % len(self.a)]
             self.j = (self.j + 1) % len(self.a)
-        else:
-            for k in range(i, self.n - 1):
+        else: # Shift elements to the left, removing from the second-half
+            for k in range(i, self.n - 1): 
                 self.a[(k + self.j) % len(self.a)] = self.a[(k + self.j + 1) % len(self.a)]
         self.n -= 1
-        if len(self.a) >= 3 * self.n:
+        if len(self.a) >= 3 * self.n: # Invariant
             self.resize()
         return x
 
